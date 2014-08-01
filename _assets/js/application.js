682244aef819f6390540e0b35a1acec8581cb096
David Pierce <david.dean.pierce@gmail.com>
2014-08-01 12:53:35 -0700

    Adds ability to sort by project position and status

--- _assets/js/application.js
+++ _assets/js/application.js
@@ -12,12 +12,17 @@
     .controller('ProjectsController',
     ['$scope', '$http', function ($scope, $http) {
       $http.get('/projects').success(function (projects) {
+        var positionCounter = 1;
+
         $scope.projects = projects.map(function (p) {
-          p.symbol = statusToSymbol(p);
+          p.Symbol = statusToSymbol(p);
+          p.Position = positionCounter;
+          positionCounter++;
           return p;
         });
       });
 
       $scope.greeting = "Hello Asana!";
+      $scope.orderProp = "Position";
     }]);
 })(window.angular);
