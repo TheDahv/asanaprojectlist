aca78f585f6d8900eac62d948fb71ec87296d358
David Pierce <david.dean.pierce@gmail.com>
2015-02-25 15:11:03 -0800

    Adds error passing/handling and logging

--- web/asana.go
+++ web/asana.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -30,70 +31,96 @@ type projectTaskDetailResponse struct {
 }
 
 func authenticatedGet(path string) ([]byte, error) {
+	fmt.Printf("authenticatedGet(%s)\n", path)
 	client := &http.Client{}
 
 	apikey := viper.GetString("asanakey")
-	req, _ := http.NewRequest("GET", "https://app.asana.com/api/1.0/"+path, nil)
+	reqPath := "https://app.asana.com/api/1.0/" + path
+	req, err := http.NewRequest("GET", reqPath, nil)
+	if err != nil {
+		fmt.Printf("Error creating request for %s: %s\n", reqPath, err.Error())
+		return nil, err
+	}
 	req.SetBasicAuth(apikey, "")
 
+	fmt.Printf("Issuing request to %s\n", reqPath)
+	if len(apikey) == 0 {
+		fmt.Printf("Authentication key is empty")
+	}
 	resp, err := client.Do(req)
 	if err != nil {
+		fmt.Printf("Error on client request %s\n", err.Error())
 		return nil, err
 	}
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading API response: %s", err.Error())
+		return nil, err
+	}
 	defer resp.Body.Close()
 
 	return body, nil
 }
 
-// GetProjects - gets all projects for the current user
-func GetProjects() []m.Project {
+// GetProjects - gets all projects for the current user or returns an error
+func GetProjects() (projects []m.Project, err error) {
+	fmt.Println("GetProjects()")
 	body, err := authenticatedGet("projects")
 	if err != nil {
-		panic(err.Error())
-	} else {
-		var responseData projectResponse
-		err = json.Unmarshal(body, &responseData)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		return responseData.Data
+		fmt.Printf("Got an error loading projects: %s", err.Error())
+		return
 	}
+
+	var responseData projectResponse
+	err = json.Unmarshal(body, &responseData)
+	if err != nil {
+		fmt.Printf("Got an error parsing unmarshalling projects response: %s\n", err.Error())
+		return
+	}
+
+	projects = responseData.Data
+
+	return
 }
 
-func getProjectTaskIDS(projectID string) []projectTaskIDs {
+func getProjectTaskIDS(projectID string) (ids []projectTaskIDs, err error) {
+	fmt.Printf("getProjectTaskIDS(%s)\n", projectID)
 	// Get list of task IDS
 	idsData, err := authenticatedGet("projects/" + projectID + "/tasks")
 	if err != nil {
-		panic(err.Error())
+		fmt.Printf("Error in project IDS API call: %s\n", err.Error())
+		return nil, err
 	}
 
 	var idsList projectTaskIdsResponse
 	err = json.Unmarshal(idsData, &idsList)
 
 	if err != nil {
-		panic(err.Error())
+		fmt.Printf("Error unmarshalling project task IDS: %s\n", err.Error())
+		return nil, err
 	}
 
-	return idsList.Data
+	ids = idsList.Data
+
+	return
 }
 
-func getProjectTaskDetail(taskID int) m.ProjectTask {
+func getProjectTaskDetail(taskID int) (task m.ProjectTask, err error) {
 	taskDetailData, err := authenticatedGet("tasks/" + strconv.Itoa(taskID))
 	if err != nil {
-		panic(err.Error())
+		return
 	}
 
 	var taskDetail projectTaskDetailResponse
 	err = json.Unmarshal(taskDetailData, &taskDetail)
 
 	if err != nil {
-		panic(err.Error())
+		return
 	}
 
-	return taskDetail.Data
+	task = taskDetail.Data
+	return
 }
 
 // GetProjectDetails - Returns the project details for the given project ID
@@ -115,32 +142,48 @@ func GetProjectDetails(projectID string) m.ProjectDetails {
 
 // GetProjectTasks - Given a project, download the list of tasks and then
 // load the task details for each
-func GetProjectTasks(projectID string) []m.ProjectTask {
+func GetProjectTasks(projectID string) (tasks []m.ProjectTask, err error) {
 	// Get list of IDs
-	idsList := getProjectTaskIDS(projectID)
+	idsList, err := getProjectTaskIDS(projectID)
+	if err != nil {
+		return
+	}
 
 	// Concurrently expand the list of project tasks
 	tasksLength := len(idsList)
 
 	type empty struct{}                  // Semaphore for timing and sequencing
 	sem := make(chan empty, tasksLength) // as we are loading tasks
+	taskErrorChan := make(chan error)
 
 	// Empty slice to hold our tasks details
-	projectTaskDetails := make([]m.ProjectTask, tasksLength)
+	tasks = make([]m.ProjectTask, tasksLength)
 
 	for i, task := range idsList {
 		// Spin up a goroutine as a closure over the
 		// results slice and loop through each task
 		go func(i int, taskID int) {
-			projectTaskDetails[i] = getProjectTaskDetail(taskID)
+			tasks[i], err = getProjectTaskDetail(taskID)
+			if err != nil {
+				taskErrorChan <- err
+			}
 			// Ping back on the channel when it is done
 			sem <- empty{}
 		}(i, task.ID)
 	}
 	// Wait for each goroutine on the channel to ping back
 	for i := 0; i < tasksLength; i++ {
-		<-sem
+		select {
+		case _ = <-sem: // Do nothing
+		case err := <-taskErrorChan:
+			fmt.Printf("Error when loading task details: %s\n", err.Error())
+			// Bail early from further processing
+			break
+		}
 	}
 
-	return projectTaskDetails
+	close(taskErrorChan)
+	close(sem)
+
+	return
 }
--- web/web.go
+++ web/web.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"code.google.com/p/gorilla/mux"
@@ -20,20 +21,34 @@ func SetupRoutes() *mux.Router {
 }
 
 func projectsHandler(w http.ResponseWriter, r *http.Request) {
-	var projects []m.Project
+	fmt.Println("projectsHandler")
+	projects := []m.Project{}
 
-	for _, project := range GetProjects() {
-		// Filter out projects that are not "top-tier" projects
-		if project.IsAProject() {
-			// Prepare virtual fields for JSON output
-			project.Prepare()
-			projects = append(projects, project)
+	response, err := GetProjects()
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		for _, project := range response {
+			// Filter out projects that are not "top-tier" projects
+			if project.IsAProject() {
+				// Prepare virtual fields for JSON output
+				project.Prepare()
+				projects = append(projects, project)
+			}
+		}
+
+		fmt.Printf("Replying with %d projects\n", len(projects))
+		output, err := json.Marshal(projects)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error marshalling projects: " + err.Error()))
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(output)
 		}
 	}
-
-	output, _ := json.Marshal(projects)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(output)
 }
 
 func projectDetailsHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,10 +67,14 @@ func projectTasksHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	projectID := vars["projectID"]
 
-	projectTasks := GetProjectTasks(projectID)
+	projectTasks, err := GetProjectTasks(projectID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		output, _ := json.Marshal(projectTasks)
 
-	output, _ := json.Marshal(projectTasks)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(output)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(output)
+	}
 }
